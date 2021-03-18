1fa710a0d2d828da256584b906916f8c22640f40
Xunxin Li <xli9400@Xunxins-MBP.lan1>
2021-03-18 12:50:44 -0400

    Finish task 3, 4 & 5

--- index.js
+++ index.js
@@ -86,6 +86,8 @@ Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
 
+console.log(reviews[5].feedback);
+
 
 
 
@@ -95,6 +97,12 @@ Using the reviews array above do the following: (no function needed)
   2. log the whole array to the console, make sure the new review is inside of it   
 */
 
+{
+  let newReview = {name:"Kyle",rating:5,feedback:"I like everything they offer"};
+  reviews.push(newReview);
+  console.log(reviews);
+}
+
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -102,6 +110,9 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
+
+reviews[reviews.length-2].feedback = "this place is chill with really cool people, great for getting work done on weekdays";
+console.log(reviews);
 
 
 
